8d9c962e719560ffa8af32b5473e32f0bfaee64f
Uli Kusterer <ulivc@zathras.de>
2021-05-08 14:41:35 +0200

    Initial Commit

---
+++ ObjectiveK2/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ObjectiveK2
+//
+//  Created by Uli Kusterer on 08.05.21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
